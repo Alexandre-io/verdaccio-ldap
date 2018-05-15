e4c8d8f40ab3911100d05e8e987c94c27578043e
Alexandre-io <Alexandre-io@users.noreply.github.com>
2018-05-15 19:07:18 +0200

    test(ldap): adding an example of ldap test

---
+++ tests/bootstrap.spec.js
@@ -0,0 +1,12 @@
+const { spawn } = require('child_process');
+let server;
+
+before(function (done) {
+    server = spawn('nodejs', ['node_modules/ldap-server-mock/server.js', '--conf=../../tests/ldap-server-mock-conf.json', '--database=../../tests/users.json']);
+    setTimeout(done, 1000);
+});
+
+after(function (done) {
+    server.kill('SIGKILL');
+    done();
+});
