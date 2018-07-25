9e9be76918fc2bd311582593bbd8fa5a4b1304eb
Alexandre-io <Alexandre-io@users.noreply.github.com>
2018-07-25 09:38:19 +0200

    fix(Auth): fix ldap cache by using a single instance of ldapauth

--- tests/integration/test.spec.js
+++ tests/integration/test.spec.js
@@ -1,29 +1,31 @@
 const Auth = require('../../index');
 const should = require('chai').should();
 const bunyan = require('bunyan');
-const log = bunyan.createLogger({ name: 'myapp' });
+const log = bunyan.createLogger({ name: 'verdaccio-ldap' });
 
-
-const auth = new Auth({
-  client_options: {
-    url: "ldap://localhost:4389",
-    searchBase: 'ou=users,dc=myorg,dc=com',
-    searchFilter: '(&(objectClass=posixAccount)(!(shadowExpire=0))(uid={{username}}))',
-    groupDnProperty: 'cn',
-    groupSearchBase: 'ou=groups,dc=myorg,dc=com',
-    // If you have memberOf:
-    searchAttributes: ['*', 'memberOf'],
-    // Else, if you don't:
-    // groupSearchFilter: '(memberUid={{dn}})',
-  }
-}, { logger: log });
 
 describe('ldap auth', function () {
   it('should match user', function (done) {
+
+    const auth = new Auth({
+      client_options: {
+        url: "ldap://localhost:4389",
+        searchBase: 'ou=users,dc=myorg,dc=com',
+        searchFilter: '(&(objectClass=posixAccount)(!(shadowExpire=0))(uid={{username}}))',
+        groupDnProperty: 'cn',
+        groupSearchBase: 'ou=groups,dc=myorg,dc=com',
+        // If you have memberOf:
+        searchAttributes: ['*', 'memberOf'],
+        // Else, if you don't:
+        // groupSearchFilter: '(memberUid={{dn}})',
+      }
+    }, { logger: log });
+
     auth.authenticate('user', 'password', function (err, results) {
       (err === null).should.be.true;
       results[0].should.equal('user');
       done();
     });
+
   });
 });
