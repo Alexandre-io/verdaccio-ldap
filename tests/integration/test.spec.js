8ef8f9a6a8de23b2b8347f5b9845b815b46bde51
Gitlab-CI <dev@kopaxgroup.com>
2018-07-25 11:00:06 +0700

    fix(test): fixing test by moving init after beforeAll

--- tests/integration/test.spec.js
+++ tests/integration/test.spec.js
@@ -3,23 +3,21 @@ const should = require('chai').should();
 const bunyan = require('bunyan');
 const log = bunyan.createLogger({ name: 'myapp' });
 
-
-const auth = new Auth({
-  client_options: {
-    url: "ldap://localhost:4389",
-    searchBase: 'ou=users,dc=myorg,dc=com',
-    searchFilter: '(&(objectClass=posixAccount)(!(shadowExpire=0))(uid={{username}}))',
-    groupDnProperty: 'cn',
-    groupSearchBase: 'ou=groups,dc=myorg,dc=com',
-    // If you have memberOf:
-    searchAttributes: ['*', 'memberOf'],
-    // Else, if you don't:
-    // groupSearchFilter: '(memberUid={{dn}})',
-  }
-}, { logger: log });
-
 describe('ldap auth', function () {
   it('should match user', function (done) {
+    const auth = new Auth({
+      client_options: {
+        url: "ldap://localhost:4389",
+        searchBase: 'ou=users,dc=myorg,dc=com',
+        searchFilter: '(&(objectClass=posixAccount)(!(shadowExpire=0))(uid={{username}}))',
+        groupDnProperty: 'cn',
+        groupSearchBase: 'ou=groups,dc=myorg,dc=com',
+        // If you have memberOf:
+        searchAttributes: ['*', 'memberOf'],
+        // Else, if you don't:
+        // groupSearchFilter: '(memberUid={{dn}})',
+      }
+    }, { logger: log });
     auth.authenticate('user', 'password', function (err, results) {
       (err === null).should.be.true;
       results[0].should.equal('user');
